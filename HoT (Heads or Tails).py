58fe2015aec7e9f191286072b1008156ac8edb1b
John FA <172921835+Jay-ohn@users.noreply.github.com>
2025-04-18 13:59:23 +0100

    Add files via upload

---
+++ HoT (Heads or Tails).py
@@ -0,0 +1,25 @@
+import random
+
+Choice = input("Heads or Tails? H/T: ").upper()
+User_choice = "HEADS" if Choice == "H" else "TAILS" \
+    if Choice == "T" else None
+
+print("\nYou chose: " + User_choice if User_choice is not None
+      else "\nInvalid Choice!")
+
+probability = random.randint(0, 1)
+if probability == 0:
+    Computer_choice = "HEADS"
+else:
+    Computer_choice = "TAILS"
+
+print("Computer chose: " + Computer_choice + "\n")
+
+if User_choice is None:
+    print("You Lose!\nChoose either H or T next time!")
+elif User_choice == Computer_choice:
+    print("You win!")
+else:
+    print("You lose!")
+
+Exit = input("\nExit...")
---
+++ RPS (Rock, Paper, Scissors).py
@@ -0,0 +1,52 @@
+import random
+import time
+
+rock = '''
+    _______
+---'   ____)
+      (_____)
+      (_____)
+      (____)
+---.__(___)
+'''
+
+paper = '''
+    _______
+---'   ____)____
+          ______)
+          _______)
+         _______)
+---.__________)
+'''
+
+scissors = '''
+    _______
+---'   ____)____
+          ______)
+       __________)
+      (____)
+---.__(___)
+'''
+
+computer_choice = random.randint(0, 2)
+user_choice = int(input("Alright! let's settle this once and for all.\n"
+                     "Rock, Paper, Scissors! What do you choose? "
+                     "\n\nType 0 for Rock, 1 for Paper, 2 for Scissors: "))
+
+if user_choice not in [0, 1, 2]:
+    print("\nInvalid choice! Please restart the game and choose a valid option.")
+    time.sleep(3)
+    exit()
+
+print("\nYou chose: ", rock if user_choice == 0 else paper if user_choice == 1 else scissors)
+
+print("Computer chose: ", rock if computer_choice == 0 else paper if computer_choice == 1 else scissors)
+
+print("You win!" if (user_choice == 0 and computer_choice == 2) or
+                    (user_choice == 1 and computer_choice == 0) or
+                    (user_choice == 2 and computer_choice == 1) else
+      "It's a draw!" if user_choice == computer_choice else
+      "You lose! Go home and practice more!")
+
+time.sleep(3)
+exit()
